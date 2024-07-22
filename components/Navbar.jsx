ac007b5873728113d74ce91897dce8d15072803d
romeokany187 <romeokany187@gmail.com>
2024-07-22 15:07:43 +0100

    rechange

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,9 +12,6 @@ const NavBar = (
         <Link href="/"><div className="text-white text-[1rem] cursor-pointer">About Us</div></Link>
         <Link href="/"><div className="text-white text-[1rem] cursor-pointer">Services</div></Link>
         <Link href="/"><div className="text-white text-[1rem] cursor-pointer">Contacts</div></Link>
-        <Link href="/"><div className="text-white text-[1rem] cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"
-          fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round"
-            strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg></div></Link>
       </div>
     </div>
   )
