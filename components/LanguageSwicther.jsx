b19d65023742e9f0c1f36f92cd4f23173af2f3e3
romeokany187 <romeokany187@gmail.com>
2024-07-23 02:53:02 +0100

    remake

---
+++ components/LanguageSwicther.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import Image from 'next/image';
+
+const LanguageSwitcher = () => {
+    const [language, setLanguage] = useState('en');
+
+    const handleLanguageChange = (event) => {
+        setLanguage(event.target.value);
+    };
+
+    const getFlagSrc = (lang) => {
+        switch (lang) {
+            case 'en':
+                return '/assets/angleterre.jpg';
+            case 'fr':
+                return '/assets/france.png';
+            default: '/assets/france.png'
+        }
+    };
+
+    return (
+        <div className="flex items-center gap-2 px-1 bg-slate-800 rounded-md z-50">
+            <Image src="/assets/Language.png" alt="Language Flag" width={17} height={24} />
+            <div className="w-[1rem] h-[1rem]">
+                <img src={getFlagSrc(language)} alt="Language Flag" className="object-fill w-full h-full rounded-[50%]" />
+            </div>
+            <select value={language} onChange={handleLanguageChange} className=" bg-slate-800
+             text-[#dce5e2] text-[1rem]   border-none rounded  py-1 outline-none">
+                <option value="en">EN</option>
+                <option value="fr">FR</option>
+            </select>
+        </div>
+    );
+};
+
+export default LanguageSwitcher;
