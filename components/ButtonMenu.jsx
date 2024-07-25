1d6ea09140c191216b5238dfcb6cb973bb9ddb7f
romeokany187 <romeokany187@gmail.com>
2024-07-25 12:05:43 +0100

    remake

--- components/ButtonMenu.jsx
+++ components/ButtonMenu.jsx
@@ -70,20 +70,20 @@ const ButtonMenu = () => {
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer">Services</div></Link>
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer">Contacts</div></Link>
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer flex gap-2">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="fill-gray-500 w-[1.4rem] ">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="fill-gray-500 w-[1.2rem] ">
                                                             <title>Mail</title>
                                                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                                                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                                                         </svg>
-                                                        <p className=" text-gray-500  ">Couriel</p>
+                                                        <p className=" text-gray-500 text-[1.2rem] ">Couriel</p>
                                                     </div>
                                                     </Link>
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer flex gap-2">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-gray-500 w-[1.4rem] ">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-gray-500 w-[1.2rem] ">
                                                             <title>Téléphone</title>
                                                             <path d="M6.62 10.79a15.466 15.466 0 0 0 6.59 6.59l2.2-2.2a1.14 1.14 0 0 1 1.16-.27c1.26.42 2.62.65 4.02.65a1.15 1.15 0 0 1 1.15 1.15v3.84a1.15 1.15 0 0 1-1.15 1.15C10.65 22 2 13.35 2 2.15A1.15 1.15 0 0 1 3.15 1h3.84a1.15 1.15 0 0 1 1.15 1.15c0 1.4.23 2.76.65 4.02.14.46.02.97-.27 1.16l-2.2 2.2z" />
                                                         </svg>
-                                                        <p className=" text-gray-500">Couriel</p>
+                                                        <p className=" text-gray-500 text-[1.2rem]">Couriel</p>
                                                     </div>
                                                     </Link>
                                                 </div>
