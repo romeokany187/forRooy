33b0d22ad38aaaf5eaec78c521f67cf0c5723340
romeokany187 <romeokany187@gmail.com>
2024-07-25 11:50:32 +0100

    remake

--- components/ButtonMenu.jsx
+++ components/ButtonMenu.jsx
@@ -51,7 +51,7 @@ const ButtonMenu = () => {
                             <>
                                 <div className="">
                                     <div className="w-full h-[90vh] flex flex-col justify-center  gap-[9rem] ">
-                                        <div className="flex gap-[15rem] mt-12 text-[1.5rem] font-bold">
+                                        <div className="flex gap-[15rem] mt-12 text-[1.7rem] font-bold">
                                             <div className="">
                                                 <p className="text-[#dce5e2]  mb-4">Bienvenue</p>
                                                 <div className="flex flex-col gap-2 ">
@@ -70,7 +70,7 @@ const ButtonMenu = () => {
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer">Services</div></Link>
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer">Contacts</div></Link>
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer flex gap-2">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="fill-gray-500 w-[1.2rem] ">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className="fill-gray-500 w-[1.4rem] ">
                                                             <title>Mail</title>
                                                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                                                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
@@ -79,7 +79,7 @@ const ButtonMenu = () => {
                                                     </div>
                                                     </Link>
                                                     <Link href="/"><div className="text-[#dce5e2]  cursor-pointer flex gap-2">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-gray-500 w-[1.2rem] ">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-gray-500 w-[1.4rem] ">
                                                             <title>Téléphone</title>
                                                             <path d="M6.62 10.79a15.466 15.466 0 0 0 6.59 6.59l2.2-2.2a1.14 1.14 0 0 1 1.16-.27c1.26.42 2.62.65 4.02.65a1.15 1.15 0 0 1 1.15 1.15v3.84a1.15 1.15 0 0 1-1.15 1.15C10.65 22 2 13.35 2 2.15A1.15 1.15 0 0 1 3.15 1h3.84a1.15 1.15 0 0 1 1.15 1.15c0 1.4.23 2.76.65 4.02.14.46.02.97-.27 1.16l-2.2 2.2z" />
                                                         </svg>
@@ -90,7 +90,7 @@ const ButtonMenu = () => {
                                             </div>
                                         </div>
                                         <div className="flex justify-between ">
-                                            <p className="text-gray-500 text-[1.5rem] ">©2024 DanRooy studio</p>
+                                            <p className="text-gray-500 text-[1rem] ">©2024 DanRooy studio</p>
                                             <div className="flex gap-5">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-gray-500 w-[1.2rem]">
                                                     <title>LinkedIn</title>
